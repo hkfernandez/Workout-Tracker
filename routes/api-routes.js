3d0c4d76c2bd43134562af318c09405804ae7efe
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-23 19:44:38 -0600

    /exeercise html route added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,34 +4,45 @@ const Workout = require("../models/workouts.js");
 
 module.exports = function(app) {
 
-	app.get("/api/workouts", (request, response) => {
-		console.log(Workout);
-		Workout.find()
-		  .then(allWorkouts => {
-			  console.log('WORKOUTS ROUTES HIT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', allWorkouts);
-			response.json(allWorkouts);
-		  })
-		  .catch(err => {
-			console.log('WORKOUTS ROUTES HIT WITH ERROR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-			response.json(err);
-		  });
-	  });
+	app.get("/api/workouts", 
+		(request, response) => {
+			Workout.find()
+			.then(
+				allWorkouts => {
+				//   console.log('WORKOUTS ROUTES HIT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', allWorkouts);
+				response.json(allWorkouts);
+				}
+			)
+			.catch(
+				err => {
+				// console.log('WORKOUTS ROUTES HIT WITH ERROR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+				response.json(err);
+			}
+			);
+		}
+	);
+
+	app.put("/api/workouts/:id", 
+		(request, response) => {
+			let workoutId = request.params
+			let newExercise = request.body
+			console.log(workoutId);
+			console.log(newExercise);
+			Workout.findOneAndUpdate({_id: workoutId}, { $push: { exercises: newExercise} }, { new: true })
+			.then(
+				updatedWorkout => {
+					response.json(updatedWorkout);
+				}
+			)
+			.catch(
+				err => {
+					console.log('WORKOUTS ROUTES HIT WITH ERROR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+					response.json(err);
+				}
+			);
+	  	}
+	);
 	
-	// const API = {
-	// async getLastWorkout() {
-	// 	let res;
-	// 	try {
-	// 	res = await fetch("/api/workouts");
-	// 	} catch (err) {
-	// 	console.log(err)
-	// 	}
-	// 	const json = await res.json();
-
-	// 	return json[json.length - 1];
-	// },
-	// async addExercise(data) {
-	// 	const id = location.search.split("=")[1];
-
 	// 	const res = await fetch("/api/workouts/" + id, {
 	// 	method: "PUT",
 	// 	headers: { "Content-Type": "application/json" },
---
+++ routes/html-routes.js
@@ -0,0 +1,65 @@
+const dotenv = require ('dotenv')
+dotenv.config();
+const Workout = require("../models/workouts.js");
+
+module.exports = function(app) {
+
+	app.get("/exercise", 
+		(request, response) => {
+			console.log('EXERCISE ROUTE HIT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+			response.redirect('exercise.html');
+		}
+	
+	);
+
+	// app.put("/api/workouts/:id", 
+	// 	(request, response) => {
+	// 		let workoutId = request.params
+	// 		let newExercise = request.body
+	// 		console.log(workoutId);
+	// 		console.log(newExercise);
+	// 		Workout.findOneAndUpdate({_id: workoutId}, { $push: { exercises: newExercise} }, { new: true })
+	// 		.then(
+	// 			updatedWorkout => {
+	// 				response.json(updatedWorkout);
+	// 			}
+	// 		)
+	// 		.catch(
+	// 			err => {
+	// 				console.log('WORKOUTS ROUTES HIT WITH ERROR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+	// 				response.json(err);
+	// 			}
+	// 		);
+	//   	}
+	// );
+	
+	// 	const res = await fetch("/api/workouts/" + id, {
+	// 	method: "PUT",
+	// 	headers: { "Content-Type": "application/json" },
+	// 	body: JSON.stringify(data)
+	// 	});
+
+	// 	const json = await res.json();
+
+	// 	return json;
+	// },
+	// async createWorkout(data = {}) {
+	// 	const res = await fetch("/api/workouts", {
+	// 	method: "POST",
+	// 	body: JSON.stringify(data),
+	// 	headers: { "Content-Type": "application/json" }
+	// 	});
+
+	// 	const json = await res.json();
+
+	// 	return json;
+	// },
+
+	// async getWorkoutsInRange() {
+	// 	const res = await fetch(`/api/workouts/range`);
+	// 	const json = await res.json();
+
+	// 	return json;
+	// },
+	// };
+}
